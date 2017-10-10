fcbe5e375fd54a3345823632c8c0e6781f53d2bc
Zack Stout <zackstout@gmail.com>
2017-10-10 16:13:51 -0500

    added a server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('server/public'));
+
+app.listen(port, function(){
+  console.log('listening on port', port);
+});
